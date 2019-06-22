ba1321d8e6d026ca17e425a132e236d16d71df25
John Kim <13jkim@gmail.com>
2019-06-22 16:17:16 -0400

    e6 completed

--- part1/e6-11/src/index.js
+++ part1/e6-11/src/index.js
@@ -1,14 +1,37 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Button = ({ text, onClick }) => (
+	<button onClick={onClick}>{text}</button>
+)
+
 const App = () => {
 	const [good, setGood] = useState(0)
 	const [neutral, setNeutral] = useState(0)
 	const [bad, setBad] = useState(0)
 
+	const handleClickGood = (val) => () => {
+		setGood(val)
+	}
+
+	const incrementNeutral = () => () => {
+		setNeutral(neutral + 1)
+	}
+
+	const incrementBad = () => () => {
+		setBad(bad + 1)
+	}
+
 	return (
 		<div>
-			code here
+			<h1>give feedback</h1>
+			<Button onClick={handleClickGood(good + 1)} text='good' />
+			<Button onClick={incrementNeutral()} text='neutral' />
+			<Button onClick={incrementBad()} text='bad' />
+			<h1> statistics</h1>
+			<p>good {good}</p>
+			<p>neutral {neutral}</p>
+			<p>bad {bad}</p>
 		</div>
 	)
 }
